50ff1d7a250a70fd3c270506d26100e192606d76
enriver <riverkeh@naver.com>
2021-02-09 12:37:20 +0900

    0209

---
+++ Reinforcement Learning/SARSA/main.py
@@ -0,0 +1,19 @@
+from QAgent import *
+from GridWorld import *
+
+if __name__=="__main__":
+    env=GridWorld()
+    agent=QAgent()
+
+    for n_epi in range(1000):
+        done=False
+
+        s=env.reset()
+        while not done:
+            a=agent.select_action(s)
+            s_prime, r, done=env.step(a)
+            agent.update_table((s,a,r,s_prime))
+            s=s_prime
+        agent.anneal_eps()
+
+    agent.show_table()
